478cfcb68624f15e19d687cf35037ba910454d0d
Sebastian Springer <info@sebastian-springer.com>
2017-03-16 18:49:58 +0100

    lessons

---
+++ Uebung1/5/index.js
@@ -0,0 +1,19 @@
+function leapYear(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+
+    if (year % 100 === 0) {
+        return false;
+    }
+
+    if (year % 4 === 0) {
+        return true;
+    }
+
+    return false;
+}
+
+
+console.log("2012: ", leapYear(2012));
+console.log("2015: ", leapYear(2015));
